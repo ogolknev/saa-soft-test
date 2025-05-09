93489051d3da15e4c04759749471cc49baf581fd
ogolknev <ogolknevnv@ya.ru>
2025-05-09 16:14:46 +0300

    Initial

---
+++ src/app/ui/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app theme="dark">
+    <v-main max-height="100dvh"> </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+.v-main {
+  overflow: hidden;
+}
+</style>
